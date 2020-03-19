c2067dfee45d461ebdced19fb70c97d9745d3daf
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-19 17:33:55 -0400

    lesson 4

--- mastery-intro/main.js
+++ mastery-intro/main.js
@@ -1,17 +1,23 @@
-//Add a description to the data object with the value "A pair of warm, fuzzy socks". Then display the description using an expression in an p element, underneath the h1.
+//Add an array of sizes to the data and use v-for to display them in a list.
 
-// pass in options
 var app = new Vue({
-  el: "#app", // put into dic app
-  // data you want to bind
-  // reactive so data is directly linked
+  el: "#app",
   data: {
     product: "Socks",
     image:
       "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
-    link:
-      "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ],
+    sizes: ["S", "M", "L", "XL", "XXL", "XXXL"]
   }
 });
-
-// can go to cosnsole and directly change it

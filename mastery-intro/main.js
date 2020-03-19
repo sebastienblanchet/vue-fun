98836e4718d0bf242c8dee29973756da3594d1f1
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-19 17:40:53 -0400

    lesson 6, skip ahead this course is aweful

--- mastery-intro/main.js
+++ mastery-intro/main.js
@@ -1,23 +1,40 @@
-//Add an array of sizes to the data and use v-for to display them in a list.
+//Create a new button and method to decrement the value of `cart`.
 
 var app = new Vue({
   el: "#app",
+  // data definitions
   data: {
     product: "Socks",
-    image:
-      "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+    image: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
     inStock: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [
-      {
+    variants: [{
         variantId: 2234,
-        variantColor: "green"
+        variantColor: "green",
+        variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
       },
       {
         variantId: 2235,
-        variantColor: "blue"
+        variantColor: "blue",
+        variantImage: 90 "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg"
       }
     ],
-    sizes: ["S", "M", "L", "XL", "XXL", "XXXL"]
+    cart: 0
+  },
+
+  // functions
+  methods: {
+    addToCart() {
+      // this will access data, similar to $scope
+      this.cart += 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    },
+    removeFromCart() {
+      if (this.cart >= 0) {
+        this.cart -= 1;
+      }
+    }
   }
 });
